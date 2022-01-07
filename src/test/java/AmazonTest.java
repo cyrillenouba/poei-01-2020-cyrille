78f9f92ef3fe81fb019020031e7c746f5cfea07d
cyrille <noubacurille@gmail.com>
2022-01-07 12:06:05 +0100

    nitendo mainpage 2 + add AmazonTest

---
+++ src/test/java/AmazonTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setup(){
+
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.fr");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("sp-cc-accept")).click();
+
+    }
+
+    @Test
+
+    public void nSwitchAddToCartPriceTest(){
+        //Arrange
+
+        String productName = "Nitendo Switch Oled";
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.searchProduct(productName);
+
+    }
+    @Test
+
+    public void machineAracletteTest(){
+        //Arrange
+
+        String productName = "Machine a raclette";
+
+        MainPage mainPage = new MainPage(driver);
+        mainPage.searchProduct(productName);
+
+    }
+    @AfterMethod
+    public void teardown(){
+
+        driver.quit();
+    }
+}
