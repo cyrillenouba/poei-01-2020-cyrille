d7c649773425ac73c3537e803d5dd89b91e5ac0d
cyrille <noubacurille@gmail.com>
2022-01-07 12:03:56 +0100

    nitendo mainpage

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,11 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class MainPage {
     WebDriver driver;
 
+    int timeoutSearch = 10;
+    By searchBArSelector = By.id("twotabsearchtextbox");
+    By searchResultSelector = By.cssSelector("[data-component-type='s-search-result']");
+
     public MainPage(WebDriver driver){
 
         this.driver=driver;
+    }
+
+    public void searchProduct(String productName){
+
+        driver.findElement(searchBArSelector).sendKeys(productName + Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutSearch)) ;
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchResultSelector));
+
     }
 }
 
--- src/main/java/SearchResultPage.java
+++ src/main/java/SearchResultPage.java
@@ -5,4 +5,6 @@ import org.openqa.selenium.WebDriver;
     public SearchResultPage(WebDriver driver){
         this.driver=driver;
     }
+
+
 }
