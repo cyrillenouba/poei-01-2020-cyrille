fde2ae1125822cabcdf174f193ee2b90e0b56e4c
cyrille <noubacurille@gmail.com>
2022-01-11 15:01:09 +0100

    selenium grid + docker

--- src/test/java/UberEatsTests.java
+++ src/test/java/UberEatsTests.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,6 +13,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 import java.util.function.Function;
@@ -24,7 +28,15 @@ public class UberEatsTests {
 
        String cookieeSelector ="#cookie-banner button.bj";
 
-        driver = new ChromeDriver();
+       // driver = new ChromeDriver();
+        URL seleniumGridUrl = null;
+        try {
+            seleniumGridUrl = new URL("http://127.0.0.1:4444");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new RemoteWebDriver(seleniumGridUrl, chromeOptions);
         driver.get("https://www.ubereats.com");
         driver.manage().window().maximize();
         //fermer cookies
